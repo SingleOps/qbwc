deecb724d9982f89c35d97ef8aaee37e6216e5f0
Adam Griffis <abgriff@gmail.com>
2018-05-05 21:07:16 -0500

    Test some fixes for pending jobs.

--- lib/qbwc/active_record/session.rb
+++ lib/qbwc/active_record/session.rb
@@ -14,7 +14,7 @@ class QBWC::ActiveRecord::Session < QBWC::Session
       # Restore current job from saved one on QbwcSession
       @current_job = QBWC.get_job(@session.current_job) if @session.current_job
       # Restore pending jobs from saved list on QbwcSession
-      @pending_jobs = refresh_pending_jobs
+      @pending_jobs = refresh_pending_jobs(@session.pending_jobs.split(','), account_id, self)
       super(@session.user, @session.company, @session.ticket, @session.account_id)
     else
       super
@@ -28,14 +28,13 @@ class QBWC::ActiveRecord::Session < QBWC::Session
     end
   end
 
-  def refresh_pending_jobs
-    @session = session_or_user
-    QBWC.get_pending_jobs_by_name(@session.pending_jobs.split(','), account_id, self)
+  def refresh_pending_jobs(pending_job_names, account_id, session)
+    QBWC.get_pending_jobs_by_name(@session.pending_jobs, account_id, self)
   end
 
   def save
-    @session.pending_jobs = refresh_pending_jobs.map(&:name).join(',')
-    @session.current_job = current_job.try(:name)
+    @session.pending_jobs = refresh_pending_jobs(pending_jobs.map(&:name), nil, self)
+    @session.current_job = current_job.reload.try(:name)
     @session.save
     super
   end
